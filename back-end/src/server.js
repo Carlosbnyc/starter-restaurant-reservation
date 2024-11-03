4e3527b4b85e2d32cec00d769f5c432ad462aaaf
Carlos <bowyercarlos@gmail.com>
2024-11-03 18:04:23 -0500

    app.js modified

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,20 +1,7 @@
 const { PORT = 5000 } = process.env;
-const express = require('express'); // Ensure express is imported
-const cors = require('cors'); // Import the cors package
 
 const app = require("./app");
 const knex = require("./db/connection");
-
-// Use CORS middleware
-app.use(cors()); // This will allow requests from any origin
-
-// If you want to restrict to only your frontend origin, use:
-// const corsOptions = {
-//   origin: 'http://localhost:3000', // Allow only this origin
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
 
 knex.migrate
   .latest()
